69c8d5388f023d3bc0f9305fb90208d9cf281755
oladapsy <dapoodedeyi@yahoo.com>
2024-10-16 18:53:20 +0100

    async_await_with_async_io

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" async coroutine """
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    ret = random.uniform(0, max_delay)
+    await asyncio.sleep(ret)
+    return ret
