560bf1e7d2a063dfd963cac0b850cc50db62c5f8
oladapsy <dapoodedeyi@yahoo.com>
2024-10-21 01:14:27 +0000

    7-correction

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -15,4 +15,4 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
         The second element is the square of the int/float
         v and should be annotated as a float.
     """
-    return (k, (v * v * v))
+    return (k, (v * v))
